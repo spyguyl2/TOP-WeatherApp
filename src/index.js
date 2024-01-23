b1faf3b7b9ef293aa11ba04db6d5e994c62ad321
Damon Haase <damon.haase@gmail.com>
2024-01-23 18:20:00 -0500

    Got things working correctly.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 import './style.css';
 import './meyerReset.css';
 
-const key = 'e050bc8b5b284f7ebda233344241501';
-/* attempt 1 fetching using .then
-fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=wellington`.toLowerCase(), { mode: 'cors' })
-  .then((response) => response.json()).then((response) => console.log(response));
-   */
+const WEATHER_API_KEY = 'e050bc8b5b284f7ebda233344241501';
+// attempt 1 fetching using .then
 /*
-async function cleanWeatherData(data) {
-  const uncleanedObject = await JSON.parse(data);
-  return uncleanedObject;
-}
- */
-
-/* attempt 2 fetching using await
-const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=wellington`.toLowerCase(), { mode: 'cors' });
-console.log(await response.json());
+fetch(`https://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=wellington`.toLowerCase(), { mode: 'cors' })
+.then((response) => response.json()).then((response) => console.log(response));
 */
-
+// attempt 2 fetching using await
+/*
+const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=wellington`.toLowerCase(), { mode: 'cors' });
+const responseObject = (await response.json());
+console.log(responseObject);
+*/
 // ^^Leaving these above for examples of each kind.
 // Now make them as a function
+
+const getData = async (cityName) => {
+  const response = await fetch(
+    `https://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${cityName}`
+      .toLowerCase(),
+    { mode: 'cors' },
+  );
+  const rawData = await response.json();
+  return rawData;
+};
+
+console.log(await getData('wellington'));
